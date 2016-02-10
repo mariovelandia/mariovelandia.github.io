7df624080f5da5499e719ede0e0b6f129b19b570
Estudiantes <Estudiantes@iMac-de-Administrador-3.local>
2016-02-10 12:43:05 -0500

    login and signup front, advanced search

--- app.php
+++ app.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
 	<link rel="icon" type="image/png" href="assets/img/favicon.png">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/theme.css">
 </head>
 <body>
